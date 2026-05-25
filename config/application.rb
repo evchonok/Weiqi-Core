8aba9ebb8bf6894eeefd41b708e45d05bd8505d5
Tiverkaeva Eva <tiverkaevaeva2005@mail.ru>
2026-05-26 01:12:20 +0300

    Реализовала хоррор-режим, но потом надо доработать

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module WeiqiCore
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.paths << Rails.root.join("app", "assets", "horror")
   end
 end
