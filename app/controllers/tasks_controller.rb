1d7e6809654b3dd83a5fbb8881608b7efa5a0cf0
Vlasova Emma <yana_mironova_2019@mail.ru>
2026-06-04 19:53:11 +0300

    все задачи добавлены

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,31 +2,18 @@ class TasksController < ApplicationController
   before_action :require_login
   before_action :set_horror_mode
 
-  # === PUBLIC ACTIONS (должны быть ДО private) ===
   def menu
     @levels = TaskLevel.order(:difficulty)
   end
 
-  # метод "Открыть задачи"
   def index
     type = TaskType.find(params[:task_type_id])
-    random_task = type.tasks.sample
-
-    if random_task
-      redirect_to task_level_task_type_task_path(params[:task_level_id], type, random_task, horror: params[:horror])
-    else
-      # Просто редирект без alert
-      redirect_to task_level_task_types_path(params[:task_level_id], horror: params[:horror])
-    end
+    pick_next_task(type, nil)
   end
 
-  # Этот метод вызывается при нажатии кнопки "->" (Дальше)
   def random
     type = TaskType.find(params[:task_type_id])
-    random_task = type.tasks.sample
-
-    # Перенаправляем на новую случайную задачу
-    redirect_to task_level_task_type_task_path(params[:task_level_id], type, random_task, horror: params[:horror])
+    pick_next_task(type, params[:except_id])
   end
 
   def show
@@ -41,8 +28,7 @@ class TasksController < ApplicationController
 
   def attempt
     @task = Task.find(params[:id])
-
-    user_moves = params[:moves]
+    user_moves = params[:moves] || []
 
     if @task.check_solution?(user_moves)
       progress = UserProgress.find_or_create_by(user: current_user, task: @task)
@@ -55,11 +41,32 @@ class TasksController < ApplicationController
     end
   end
 
-  # === PRIVATE METHODS (только вспомогательные) ===
   private
 
   def set_horror_mode
     @horror_mode = params[:horror] == "true"
+  end
+
+  def pick_next_task(type, exclude_id)
+    # 1. Исключаем все решённые задачи текущего пользователя
+    solved_ids = UserProgress.where(user: current_user, is_solved: true).pluck(:task_id)
+    exclude_ids = solved_ids + [ exclude_id.to_i ].compact
+
+    available = type.tasks.where.not(id: exclude_ids)
+
+    # 2. Если всё решено, разрешаем повтор, но исключаем текущую задачу
+    if available.empty?
+      available = type.tasks.where.not(id: exclude_id)
+      flash[:notice] = "🎉 Все задачи решены! Показываем случайную из пройденных."
+    end
+
+    task = available.sample
+
+    if task
+      redirect_to task_level_task_type_task_path(params[:task_level_id], type, task, horror: params[:horror])
+    else
+      redirect_to task_level_task_types_path(params[:task_level_id], horror: params[:horror]), alert: "В этом разделе пока нет задач."
+    end
   end
 
   def parse_board_state(raw)
--- db/seeds/tasks.rb
+++ db/seeds/tasks.rb
@@ -1,56 +1,51 @@
 # db/seeds/tasks.rb
-puts "🌱 Создаю встроенные задачи (без внешних файлов)..."
+puts "🌱 Загружаю задачи по уровням..."
+TaskType.where(name: "Эндшпиль").destroy_all
 
-# 1. Уровни
+# ==========================================
+# УРОВЕНЬ 1: НОВИЧОК
+# ==========================================
 level1 = TaskLevel.find_or_create_by!(name: "Новичок") do |l|
   l.difficulty = 1
-  l.description = "Базовые задачи: захват, соединение, жизнь"
+  l.description = "Базовые задачи: жизнь, смерть, простые формы"
 end
 
-# 2. Типы
-type1 = TaskType.find_or_create_by!(task_level: level1, name: "Жизнь и смерть") do |t|
+type1_l1 = TaskType.find_or_create_by!(task_level: level1, name: "Жизнь и смерть") do |t|
   t.instructions = "Сделай группу живой или убей врага"
 end
 
-type2 = TaskType.find_or_create_by!(task_level: level1, name: "Тесудзи") do |t|
+type2_l1 = TaskType.find_or_create_by!(task_level: level1, name: "Тесудзи") do |t|
   t.instructions = "Найди красивый тактический удар"
 end
 
-# 3. Очищаем старые задачи типа "Жизнь и смерть"
-Task.where(task_type: type1).destroy_all
+# Очищаем старые задачи этого типа
+Task.where(task_type: type1_l1).destroy_all
 
-# === ЗАДАЧА 1: Жизнь группы в центре (9x9) ===
-# Решение: E5 - ключевая точка для создания глаза
-Task.find_or_create_by!(
-  task_type: type1, solution: "E5") do |t|
+# 1. Новичок - Жизнь и смерть
+
+Task.find_or_create_by!(task_type: type1_l1, solution: "E5") do |t|
   t.board_state = {
-    # Чёрные камни (окружение)
     "1,6" => "black", "3,6" => "black", "4,6" => "black", "5,6" => "black", "6,6" => "black",
     "2,5" => "black", "7,5" => "black",
     "1,4" => "black", "7,4" => "black",
     "1,3" => "black", "7,3" => "black",
     "2,2" => "black", "3,2" => "black", "4,2" => "black", "5,2" => "black", "6,2" => "black",
-    # Белые камни (группа для спасения)
     "3,5" => "white", "4,5" => "white", "5,5" => "white", "6,5" => "white",
     "2,4" => "white", "6,4" => "white",
     "3,3" => "white", "6,3" => "white",
     "3,2" => "white", "4,2" => "white", "5,2" => "white", "6,2" => "white"
   }.to_json
-  t.solution = "E5" # Ключевая точка для жизни
+  t.solution = "E5"
   t.time_limit_sec = 60
   t.points = 15
   t.horror_enabled = true
   t.hint = "Займи центральный пункт для создания двух глаз"
 end
 
-
-Task.find_or_create_by!(
-  task_type: type1, solution: "A1") do |t|
+Task.find_or_create_by!(task_type: type1_l1, solution: "A1") do |t|
   t.board_state = {
-    # Чёрные камни
     "0,3" => "black", "1,3" => "black", "2,3" => "black", "3,3" => "black",
     "4,0" => "black", "4,1" => "black", "4,2" => "black",
-    # Белые камни
     "0,2" => "white",
     "1,2" => "white", "1,1" => "white",
     "2,1" => "white", "2,0" => "white"
@@ -61,15 +56,12 @@ Task.find_or_create_by!(
   t.hint = "Защитись сверху, чтобы создать пространство для глаз"
 end
 
-# Решение: Q16 - ключевой пункт
-Task.find_or_create_by!(task_type: type1, solution: "P16") do |t|
+Task.find_or_create_by!(task_type: type1_l1, solution: "Q16") do |t|
   t.board_state = {
-    # Чёрные камни (окружение)
     "13,17" => "black", "14,17" => "black", "15,17" => "black", "16,17" => "black",
     "12,16" => "black", "17,16" => "black",
     "12,15" => "black",
     "12,14" => "black", "13,13" => "black", "14,13" => "black", "15,13" => "black", "16,13" => "black", "17,13" => "black",
-    # Белые камни (группа)
     "14,16" => "white", "15,16" => "white", "16,16" => "white",
     "13,15" => "white", "17,15" => "white",
     "14,14" => "white", "15,14" => "white", "16,14" => "white", "17,14" => "white"
@@ -81,22 +73,21 @@ Task.find_or_create_by!(task_type: type1, solution: "P16") do |t|
 end
 
 
-# === ЗАДАЧИ ДЛЯ ТИПА "АТАКА" ===
-
-# Задача 4: Атака слабой группы
-Task.find_or_create_by!(task_type: type2, solution: "R4") do |t|
+Task.find_or_create_by!(task_type: type1_l1, solution: "S2") do |t|
   t.board_state = {
-    "16,1" => "black", "15,2" => "black", "14,2" => "black", "16,4" => "black",
-    "16,2" => "white", "15,3" => "white", "14,3" => "white", "14,5" => "black"
+    # Чёрные
+    "16,0" => "black", "16,1" => "black", "16,2" => "black", "16,3" => "black", "17,3" => "black", "18,2" => "black",
+    # Белые
+    "15,1" => "white", "15,2" => "white", "15,3" => "white", "15,4" => "white", "16,4" => "white",
+    "17,4" => "white", "18,4" => "white", "18,3" => "white"
   }.to_json
   t.time_limit_sec = 45
-  t.points = 15
+  t.points = 10
   t.horror_enabled = true
-  t.hint = "Соединитесь, порезав соперника"
+  t.hint = "Важный пункт для захвата или спасения"
 end
 
-# Задача 5:
-Task.find_or_create_by!(task_type: type2, solution: "G5") do |t|
+Task.find_or_create_by!(task_type: type2_l1, solution: "G5") do |t|
   t.board_state = {
     "5,1" => "white", "5,2" => "white", "7,4" => "white", "7,5" => "white", "6,5" => "white",
     "6,2" => "black", "7,2" => "black", "5,3" => "black",
@@ -108,8 +99,230 @@ Task.find_or_create_by!(task_type: type2, solution: "G5") do |t|
   t.hint = "Постройте хорошую форму"
 end
 
+Task.find_or_create_by!(task_type: type2_l1, solution: "A17") do |t|
+  t.board_state = {
+    # Белые
+    "0,14" => "white", "1,14" => "white", "2,14" => "white", "3,14" => "white",
+    "3,15" => "white", "2,16" => "white", "2,17" => "white", "1,17" => "white", "0,17" => "white",
+    # Чёрные
+    "1,15" => "black", "2,15" => "black", "3,16" => "black", "3,17" => "black",
+    "3,18" => "black", "2,18" => "black", "1,18" => "black"
+  }.to_json
+  t.time_limit_sec = 60
+  t.points = 15
+  t.horror_enabled = true
+  t.hint = "Ключевая точка для соединения или отсечения"
+end
+
+
+Task.find_or_create_by!(task_type: type2_l1, solution: "T4") do |t|
+  t.board_state = {
+    # Белые
+    "15,2" => "white", "16,2" => "white", "17,2" => "white", "18,2" => "white",
+    "14,3" => "white", "14,4" => "white", "14,5" => "white", "15,5" => "white",
+    "16,5" => "white", "17,6" => "white", "16,7" => "white", "18,7" => "white", "18,5" => "white",
+    # Чёрные
+    "14,1" => "black", "15,1" => "black", "16,1" => "black", "17,1" => "black", "18,1" => "black",
+    "14,2" => "black", "15,3" => "black", "15,4" => "black", "16,4" => "black",
+    "17,4" => "black", "17,5" => "black"
+  }.to_json
+  t.time_limit_sec = 60
+  t.points = 15
+  t.horror_enabled = true
+  t.hint = "Удар по форме соперника"
+end
+
+
+# ==========================================
+# УРОВЕНЬ 2: ПРОДВИНУТЫЙ (СРЕДНИЙ)
+# ==========================================
+level2 = TaskLevel.find_or_create_by!(name: "Продвинутый") do |l|
+  l.difficulty = 5
+  l.description = "Задачи средней сложности"
+end
+
+type1_l2 = TaskType.find_or_create_by!(task_level: level2, name: "Жизнь и смерть") do |t|
+  t.instructions = "Сделай группу живой или убей врага"
+end
+type2_l2 = TaskType.find_or_create_by!(task_level: level2, name: "Тесудзи") do |t|
+  t.instructions = "Найди красивый тактический удар"
+end
+
+Task.where(task_type: type1_l2).destroy_all
+Task.where(task_type: type2_l2).destroy_all
+
+# 2. Продвинутый - Жизнь и смерть
+Task.find_or_create_by!(task_type: type1_l2, solution: "S1") do |t|
+  t.board_state = {
+    # Белые
+    "14,0" => "white", "14,1" => "white", "15,1" => "white", "16,1" => "white", "16,2" => "white",
+    "16,3" => "white", "17,3" => "white", "18,3" => "white",
+    # Чёрные
+    "13,1" => "black", "13,2" => "black", "15,3" => "black", "15,4" => "black", "16,4" => "black",
+    "17,4" => "black", "16,0" => "black", "17,1" => "black", "18,1" => "black"
+  }.to_json
+  t.time_limit_sec = 60
+  t.points = 20
+  t.horror_enabled = true
+  t.hint = "Ключевая точка на первой линии"
+end
+
+# 3. Продвинутый - Жизнь и смерть
+Task.find_or_create_by!(task_type: type1_l2, solution: "A2") do |t|
+  t.board_state = {
+    # Белые
+    "1,0" => "white", "1,1" => "white", "1,2" => "white", "1,3" => "white", "0,3" => "white",
+    "2,2" => "white", "2,3" => "white", "3,2" => "white", "4,1" => "white", "4,0" => "white",
+    # Чёрные
+    "0,2" => "black", "0,4" => "black", "1,4" => "black", "2,4" => "black", "3,4" => "black",
+    "3,3" => "black", "4,2" => "black", "5,2" => "black", "5,1" => "black", "5,0" => "black",
+    "2,1" => "black", "3,1" => "black"
+  }.to_json
+  t.time_limit_sec = 75
+  t.points = 25
+  t.horror_enabled = true
+  t.hint = "Узкое место в углу"
+end
+
+# 4. Продвинутый - Тесудзи
+Task.find_or_create_by!(task_type: type2_l2, solution: "S14") do |t|
+  t.board_state = {
+    # Белые
+    "15,10" => "white", "16,10" => "white", "16,11" => "white", "17,11" => "white",
+    "15,12" => "white", "15,13" => "white", "15,14" => "white", "17,14" => "white",
+    "16,15" => "white", "17,15" => "white", "14,15" => "white", "13,16" => "white",
+    "14,16" => "white", "15,16" => "white", "13,17" => "white", "14,17" => "white",
+    # Чёрные
+    "14,11" => "black", "14,12" => "black", "14,13" => "black", "14,14" => "black", "14,15" => "black",
+    "15,15" => "black", "16,14" => "black", "18,15" => "black", "16,16" => "black",
+    "16,17" => "black", "15,17" => "black", "17,16" => "black"
+  }.to_json
+  t.time_limit_sec = 90
+  t.points = 30
+  t.horror_enabled = true
+  t.hint = "Тактический удар в центре формы"
+end
+
+# 5. Продвинутый - Тесудзи
+Task.find_or_create_by!(task_type: type2_l2, solution: "S16") do |t|
+  t.board_state = {
+    # Белые
+    "15,12" => "white", "16,12" => "white", "17,12" => "white", "18,12" => "white",
+    "13,13" => "white", "14,14" => "white", "15,14" => "white", "16,15" => "white",
+    "14,15" => "white", "15,16" => "white",
+    # Чёрные
+    "14,13" => "black", "15,13" => "black", "16,13" => "black", "17,13" => "black",
+    "15,15" => "black", "16,16" => "black", "14,17" => "black", "15,17" => "black", "16,17" => "black"
+  }.to_json
+  t.time_limit_sec = 90
+  t.points = 30
+  t.horror_enabled = true
+  t.hint = "Прорыв или соединение?"
+end
+
+# 6. Продвинутый - Тесудзи
+Task.find_or_create_by!(task_type: type2_l2, solution: "S5") do |t|
+  t.board_state = {
+    # Белые
+    "16,1" => "white", "17,1" => "white", "15,2" => "white", "15,3" => "white",
+    "16,3" => "white", "17,3" => "white", "13,3" => "white", "11,3" => "white",
+    "17,5" => "white", "17,6" => "white", "16,6" => "white", "15,6" => "white",
+    # Чёрные
+    "18,1" => "black", "16,2" => "black", "17,2" => "black", "18,3" => "black",
+    "16,4" => "black", "16,5" => "black", "14,4" => "black", "13,4" => "black",
+    "14,6" => "black", "14,7" => "black", "16,8" => "black", "17,9" => "black"
+  }.to_json
+  t.time_limit_sec = 90
+  t.points = 30
+  t.horror_enabled = true
+  t.hint = "Удар по слабому месту"
+end
+
+
+# ==========================================
+# УРОВЕНЬ 3: МАСТЕР
+# ==========================================
+level3 = TaskLevel.find_or_create_by!(name: "Мастер") do |l|
+  l.difficulty = 10
+  l.description = "Сложные задачи для опытных"
+end
+
+type1_l3 = TaskType.find_or_create_by!(task_level: level3, name: "Жизнь и смерть") do |t|
+  t.instructions = "Сделай группу живой или убей врага"
+end
+type2_l3 = TaskType.find_or_create_by!(task_level: level3, name: "Тесудзи") do |t|
+  t.instructions = "Найди красивый тактический удар"
+end
+
+Task.where(task_type: type1_l3).destroy_all
+Task.where(task_type: type2_l3).destroy_all
+
+# 7. Мастер - Жизнь и смерть
+Task.find_or_create_by!(task_type: type1_l3, solution: "C1") do |t|
+  t.board_state = {
+    # Чёрные
+    "1,0" => "black", "2,1" => "black", "1,2" => "black", "1,3" => "black", "2,4" => "black",
+    "3,4" => "black", "3,3" => "black", "3,2" => "black", "4,2" => "black", "4,1" => "black", "5,0" => "black",
+    # Белые
+    "0,1" => "white", "0,2" => "white", "0,3" => "white", "1,4" => "white", "1,5" => "white",
+    "2,5" => "white", "3,5" => "white", "4,5" => "white", "4,4" => "white", "4,3" => "white",
+    "5,2" => "white", "5,1" => "white", "6,0" => "white", "7,0" => "white", "3,0" => "white", "3,1" => "white"
+  }.to_json
+  t.time_limit_sec = 120
+  t.points = 50
+  t.horror_enabled = true
+  t.hint = "Жизненно важный пункт (Vital point)"
+end
+
+# 8. Мастер - Жизнь и смерть
+Task.find_or_create_by!(task_type: type1_l3, solution: "D1") do |t|
+  t.board_state = {
+    # Чёрные
+    "1,0" => "black", "0,1" => "black", "1,2" => "black", "2,2" => "black", "3,2" => "black",
+    "4,1" => "black", "4,0" => "black",
+    # Белые
+    "0,2" => "white", "0,3" => "white", "1,3" => "white", "2,3" => "white", "3,3" => "white",
+    "4,2" => "white", "5,2" => "white", "6,1" => "white", "3,1" => "white", "2,0" => "white"
+  }.to_json
+  t.time_limit_sec = 120
+  t.points = 50
+  t.horror_enabled = true
+  t.hint = "Первая линия решает"
+end
+
+# 9. Мастер - Тесудзи
+Task.find_or_create_by!(task_type: type2_l3, solution: "A3") do |t|
+  t.board_state = {
+    # Белые
+    "0,0" => "white", "1,1" => "white", "2,1" => "white", "4,0" => "white", "4,1" => "white",
+    "4,2" => "white", "3,3" => "white", "3,4" => "white", "2,4" => "white", "1,4" => "white", "0,4" => "white",
+    # Чёрные
+    "2,0" => "black", "3,0" => "black", "3,1" => "black", "3,2" => "black", "2,2" => "black",
+    "2,3" => "black", "1,3" => "black"
+  }.to_json
+  t.time_limit_sec = 120
+  t.points = 50
+  t.horror_enabled = true
+  t.hint = "Хитрый ход в углу"
+end
+
+# 10. Мастер - Тесудзи
+Task.find_or_create_by!(task_type: type2_l3, solution: "S2") do |t|
+  t.board_state = {
+    # Белые
+    "14,0" => "white", "14,1" => "white", "14,2" => "white", "15,2" => "white", "16,2" => "white",
+    "17,3" => "white", "18,2" => "white",
+    # Чёрные
+    "13,0" => "black", "13,1" => "black", "13,2" => "black", "13,3" => "black", "14,3" => "black",
+    "15,3" => "black", "16,3" => "black", "17,4" => "black", "18,4" => "black", "15,0" => "black", "18,0" => "black"
+  }.to_json
+  t.time_limit_sec = 120
+  t.points = 50
+  t.horror_enabled = true
+  t.hint = "Тактический размен"
+end
+
 puts "✅ Готово! Загружено:"
 puts "  - #{TaskLevel.count} уровней"
 puts "  - #{TaskType.count} типов задач"
-puts "  - #{Task.where(task_type: type1).count} задач 'Жизнь и смерть'"
-puts "  - #{Task.where(task_type: type2).count} задач 'Атака'"
+puts "  - Всего задач: #{Task.count}"
